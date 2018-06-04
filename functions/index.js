1b0139bdbede9d9655e6d79cdb95c9e0040a1146
Francisco Rubio <francisrubio06@hotmail.com>
2018-06-04 12:25:02 -0500

    consultarAlojamientoEnElArea

--- functions/index.js
+++ functions/index.js
@@ -53,9 +53,50 @@ function consultTouristAttractions(request, response) {
     });
 }
 
+// Funcion para consultar los servicios de alojamiento.
+function consultarServicioAlojamiento(request, response, categoria) {
+    // Creamos una variable que  apunta al nodo "servicio".
+    var ref = admin.database().ref("servicio");
+
+    // Buscamos todos los servicios que sean de categoria alojamiento 
+    ref.orderByChild("categoria").equalTo(categoria).on("value", (snapshot) => {
+        // Para almacenar todos los datos encontrados.
+        let jsonResult = {};
+        var listaAlojamiento = {}
+        snapshot.forEach((childSnapshot, index, arr) => { // Recorremos el resultado de la busqueda.
+            var values = childSnapshot.val(); // Obtenemos un JSON con todos los valores consultados.
+            values.key = childSnapshot.key; // Almacenamos la clave del servicio en una variable.
+            // Se guardan los valores obtenidos en un arreglo.
+            listaAlojamiento[values.key] = (childSnapshot.val());           
+        }); 
+        let resultToSendDialogflow = ""; // Variable para enviar el resultado a Dialogflow.
+        if (Object.keys(listaAlojamiento).length === 0) {
+            // Enviamos un mensaje de que no se encontro ningun valor con el parametro dado por el usuario.
+            resultToSendDialogflow = "Lo siento, no pude encontrar informacion necesaria para poder responder tu duda.";
+            sendResponseToDialogflow(response, resultToSendDialogflow, listaAlojamiento); // Enviamos el resultado a Dialogflow.
+        } else {
+            // Enviamos los valores da la consulta a Dialogflow.
+            resultToSendDialogflow = "Encontré esta información sobre los servicios de alojamiento";
+            sendResponseToDialogflow(response, resultToSendDialogflow, listaAlojamiento); // Enviamos el resultado a Dialogflow.
+        }     
+    });
+}
+
 exports.virtualAssistantLatacungaWebhook = functions.https.onRequest((request, response) => {
-
-    // Llamamos a la funcion para consultar atractivos y enviamos request y response.
-    consultTouristAttractions(request, response); 
-
+    // Obtenemos la acción solicitada de la solicitud de DialogFlow
+    let accion = request.body.result.action;
+    // Revisamos la accion para llamar a la funcion correcta
+    switch (accion) {
+        case "churchInformationAction":
+        // Llamamos a la funcion para consultar atractivos y enviamos request y response.
+        consultTouristAttractions(request, response);
+        break
+        case "consultarAlojamientoEnElArea":
+        // Llamamos a la funcion para consultar atractivos y enviamos request y response.
+        consultarServicioAlojamiento(request, response, "Alojamiento");
+        break
+        default:
+        // En caso de que niguna accion sea identificada.
+        sendResponseToDialogflow(response, "La acción no fue identificada", jsonResult);
+    }
 });
