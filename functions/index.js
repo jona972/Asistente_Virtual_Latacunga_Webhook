a7181a2b0e539ec4775a512d0d87ff234740571a
Francisco Rubio <francisrubio06@hotmail.com>
2018-06-14 16:52:38 -0500

    Se añadio la fuccion consultar servicio por alias

--- functions/index.js
+++ functions/index.js
@@ -17,29 +17,36 @@ function sendResponseToDialogflow(response, result, resultArray) {
     return response.send(JSON.stringify({ 'speech': result, 'displayText': result, 'data': resultArray }));
 }
 
-// Funcion para consultar los atractivos por parametro obtenido de Dialogflow.
-function consultTouristAttractions(request, response, action) {
-
-    // Para consultar el parametro que envia Dialogflow sobre la informacion sobre las iglesias.
-    var parameterAttractive = [];
-
-    if (action === "churchInformationAction") {
-        parameterAttractive.push(request.body.result.parameters.name_churches);
-    } else if (action === "churchShowLocationAction") {
+// Funcion para obtener los parametros enviados por Dialogflow
+function getDialogflowParameters(request, action) {
+    var parameters = [];
+    switch(action) {
+        case "churchInformationAction":
+        parameters.push(request.body.result.parameters.name_churches);
+        break
+        case "hotelInformationAction":
+        parameters.push(request.body.result.parameters.name_accommodation);
+        break
+        case "churchShowLocationAction":
         var arrayContext = request.body.result.contexts;
-
         arrayContext.forEach(objectContext => {
-            parameterAttractive.push(objectContext.parameters.name_churches);
+        parameters.push(objectContext.parameters.name_churches);
         });
+        break
     }
+    return parameters;
+}
 
+// Funcion para consultar los atractivos por parametro obtenido de Dialogflow.
+function getTouristAttractionByAlias(request, response, action) {
+    var parameters = getDialogflowParameters(request, action); // Obtenemos los parametros de Dialogflow
+    Console.log("PARAMETRO:" + parameters[0]);
+    
     var ref = admin.database().ref("atractivo"); // Creamos una variable que contiene el nodo "atractivo".
 
     // Buscamos todos los datos que sean igual al alias definido en la base de datos con el parametro obtenido de Dialogflow. 
-    ref.orderByChild("alias").equalTo(parameterAttractive[0]).on("value", (snapshot) => {
-        
+    ref.orderByChild("alias").equalTo(parameters[0]).on("value", (snapshot) => {
         let jsonResult = {}; // Para almacenar todos los datos encontrados con el parametro y almacenarlo con su key respectivo.
-
         snapshot.forEach((childSnapshot) => { // Recorremos el resultado de la busqueda.
             var values = childSnapshot.val(); // Obtenemos un JSON con todos los valores consultados.
             values.key = childSnapshot.key; // Almacenamos la clave del atractivo en una variable.
@@ -49,7 +56,6 @@ function consultTouristAttractions(request, response, action) {
         }); 
 
         let resultToSendDialogflow = ""; // Variable para enviar el resultado a Dialogflow.
-
         if (Object.keys(jsonResult).length === 0) {
             // Enviamos un mensaje de que no se encontro ningun valor con el parametro dado por el usuario.
             resultToSendDialogflow = "Asegurese que el nombre del atractivo este bien ingresado, o talvez esté " +
@@ -58,10 +64,10 @@ function consultTouristAttractions(request, response, action) {
         } else {
             // Enviamos los valores da la consulta a Dialogflow.
             if (action === "churchInformationAction") {
-                resultToSendDialogflow = "¿Deseas saber la ruta de la iglesia " + parameterAttractive[0] + "?";
+                resultToSendDialogflow = "¿Deseas saber la ruta de la iglesia " + parameters[0] + "?";
                 sendResponseToDialogflow(response, resultToSendDialogflow, jsonResult); // Enviamos el resultado a Dialogflow.
             } else if (action === "churchShowLocationAction") {
-                resultToSendDialogflow = "Consultado con exito la iglesia " + parameterAttractive[0];
+                resultToSendDialogflow = "Consultado con exito la iglesia " + parameters[0];
                 sendResponseToDialogflow(response, resultToSendDialogflow, jsonResult); // Enviamos el resultado a Dialogflow.
             }
             
@@ -69,16 +75,14 @@ function consultTouristAttractions(request, response, action) {
     });
 }
 
-// Funcion para consultar los servicios por categoria.
-function consultarServiciosPorCategoria(request, response, categoria) {
-    // Creamos una variable que  apunta al nodo "servicio".
-    var ref = admin.database().ref("servicio");
+// Funcion para consultar los servicios por parametro obtenido de Dialogflow.
+function getServiceByAlias(request, response, action) {
+    var parameters = getDialogflowParameters(request, action); // Obtenemos los parametros de Dialogflow
+    var ref = admin.database().ref("servicio"); // Creamos una variable que  apunta al nodo "servicio".
 
-    // Buscamos todos los servicios que sean de la categoria buscada 
-    ref.orderByChild("categoria").equalTo(categoria).on("value", (snapshot) => {
-        // Para almacenar todos los datos encontrados.
-        let jsonResult = {};
-        var listaServicio = {}
+    // Buscamos todos los datos que sean igual al alias definido en la base de datos con el parametro obtenido de Dialogflow. 
+    ref.orderByChild("alias").equalTo(parameters[0]).on("value", (snapshot) => {
+        var listaServicio = {} // Para almacenar todos los datos encontrados.
         snapshot.forEach((childSnapshot, index, arr) => { // Recorremos el resultado de la busqueda.
             var values = childSnapshot.val(); // Obtenemos un JSON con todos los valores consultados.
             values.key = childSnapshot.key; // Almacenamos la clave del servicio en una variable.
@@ -88,7 +92,35 @@ function consultarServiciosPorCategoria(request, response, categoria) {
         let resultToSendDialogflow = ""; // Variable para enviar el resultado a Dialogflow.
         if (Object.keys(listaServicio).length === 0) {
             // Enviamos un mensaje de que no se encontro ningun valor con el parametro dado por el usuario.
-            resultToSendDialogflow = "Lo siento, no pude encontrar información necesaria para poder responder tu duda.";
+            resultToSendDialogflow = "Lo siento, no pude encontrar la información necesaria para responder tu duda."+
+            " Por favor, asegúrese de que el nombre del servicio esté ingresado correctamente."+
+            " O tal vez, no está en nuestra base de datos.";
+            sendResponseToDialogflow(response, resultToSendDialogflow, listaServicio); // Enviamos el resultado a Dialogflow.
+        } else {
+            // Enviamos los valores da la consulta a Dialogflow.
+            resultToSendDialogflow = "Esta es la información que pude encontrar sobre el servicio " + parameters[0];
+            sendResponseToDialogflow(response, resultToSendDialogflow, listaServicio); // Enviamos el resultado a Dialogflow.
+        }  
+    });
+}
+
+// Funcion para consultar los servicios por categoria.
+function getServicesByCategoria(request, response, categoria) {
+    var ref = admin.database().ref("servicio"); // Creamos una variable que  apunta al nodo "servicio".
+
+    // Buscamos todos los servicios que sean de la categoria buscada 
+    ref.orderByChild("categoria").equalTo(categoria).on("value", (snapshot) => {
+        var listaServicio = {} // Para almacenar todos los datos encontrados.
+        snapshot.forEach((childSnapshot, index, arr) => { // Recorremos el resultado de la busqueda.
+            var values = childSnapshot.val(); // Obtenemos un JSON con todos los valores consultados.
+            values.key = childSnapshot.key; // Almacenamos la clave del servicio en una variable.
+            // Se guardan los valores obtenidos en un arreglo.
+            listaServicio[values.key] = (childSnapshot.val());           
+        }); 
+        let resultToSendDialogflow = ""; // Variable para enviar el resultado a Dialogflow.
+        if (Object.keys(listaServicio).length === 0) {
+            // Enviamos un mensaje de que no se encontro ningun valor con el parametro dado por el usuario.
+            resultToSendDialogflow = "Lo siento, no pude encontrar la información necesaria para responder tu duda.";
             sendResponseToDialogflow(response, resultToSendDialogflow, listaServicio); // Enviamos el resultado a Dialogflow.
         } else {
             // Enviamos los valores da la consulta a Dialogflow.
@@ -99,15 +131,12 @@ function consultarServiciosPorCategoria(request, response, categoria) {
 }
 
 // Funcion para consultar los atractivos.
-function consultarAtractivos(request, response) {
-    // Creamos una variable que  apunta al nodo "atractivo".
-    var ref = admin.database().ref("atractivo");
+function getTouristAttraction(request, response) {
+    var ref = admin.database().ref("atractivo"); // Creamos una variable que  apunta al nodo "atractivo".
 
     // Buscamos todos los atractivos ordenados por categoria
     ref.orderByChild("categoria").on("value", (snapshot) => {
-        // Para almacenar todos los datos encontrados.
-        let jsonResult = {};
-        var listaAtractivo = {}
+        var listaAtractivo = {} // Para almacenar todos los datos encontrados.
         snapshot.forEach((childSnapshot, index, arr) => { // Recorremos el resultado de la busqueda.
             var values = childSnapshot.val(); // Obtenemos un JSON con todos los valores consultados.
             values.key = childSnapshot.key; // Almacenamos la clave del atractivo en una variable.
@@ -117,7 +146,7 @@ function consultarAtractivos(request, response) {
         let resultToSendDialogflow = ""; // Variable para enviar el resultado a Dialogflow.
         if (Object.keys(listaAtractivo).length === 0) {
             // Enviamos un mensaje de que no se encontro ningun valor con el parametro dado por el usuario.
-            resultToSendDialogflow = "Lo siento, no pude encontrar información necesaria para poder responder tu duda.";
+            resultToSendDialogflow = "Lo siento, no pude encontrar la información necesaria para responder tu duda.";
             sendResponseToDialogflow(response, resultToSendDialogflow, listaAtractivo); // Enviamos el resultado a Dialogflow.
         } else {
             // Enviamos los valores da la consulta a Dialogflow.
@@ -134,23 +163,27 @@ exports.virtualAssistantLatacungaWebhook = functions.https.onRequest((request, r
     switch (accion) {
         case "churchInformationAction":
         // Llamamos a la funcion para consultar atractivos y enviamos request y response.
-        consultTouristAttractions(request, response, "churchInformationAction");
+        getTouristAttractionByAlias(request, response, "churchInformationAction");
         break
-        case "consultarAtractivoEnElArea":
-        // Llamamos a la funcion para consultar atractivos y enviamos request y response.
-        consultarAtractivos(request, response);
-        break
-        case "consultarAlojamientoEnElArea":
+        case "hotelInformationAction":
         // Llamamos a la funcion para consultar servicios y enviamos request y response.
-        consultarServiciosPorCategoria(request, response, "Alojamiento");
-        break
-        case "consultarComidaYBebidaEnElArea":
-        // Llamamos a la funcion para consultar servicios y enviamos request y response.
-        consultarServiciosPorCategoria(request, response, "Comidas y bebidas");
+        getServiceByAlias(request, response, "hotelInformationAction");
         break
         case "churchShowLocationAction":
         // Llamamos a la funcion para consultar atractivos y enviamos request y response.
-        consultTouristAttractions(request, response, "churchShowLocationAction");
+        getTouristAttractionByAlias(request, response, "churchShowLocationAction");
+        break
+        case "consultarAtractivoEnElArea":
+        // Llamamos a la funcion para consultar atractivos y enviamos request y response.
+        getTouristAttraction(request, response);
+        break
+        case "consultarAlojamientoEnElArea":
+        // Llamamos a la funcion para consultar servicios y enviamos request y response.
+        getServicesByCategoria(request, response, "Alojamiento");
+        break
+        case "consultarComidaYBebidaEnElArea":
+        // Llamamos a la funcion para consultar servicios y enviamos request y response.
+        getServicesByCategoria(request, response, "Comidas y bebidas");
         break
         default:
         // En caso de que niguna accion sea identificada.
